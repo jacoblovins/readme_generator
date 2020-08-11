cb946976e15b03f73e6b3e14f024c9940355bd55
jacoblovins <lovins.jacob@yahoo.com>
2020-08-11 11:16:26 -0400

    cleaned up code with async await

--- index.js
+++ index.js
@@ -1,72 +1,67 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-async function createReadme() {
-    const getAnswers = await inquirer.prompt([
-        {
-            type: "input",
-            message: "Title:",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Description:",
-            name: "description"
-        },
-        {
-            type: "input",
-            message: "Installation Instructions:",
-            name: "installation",
-            default: "npm i"
-        },
-        {
-            type: "input",
-            message: "Usage Information:",
-            name: "usage"
-        },
-        {
-            type: "list",
-            message: "License:",
-            name: "license",
-            choices: ["MIT", "APACHE 2.0", "GPLv3", "BSD 3--Clause", "none"]
-        },
-        {
-            type: "input",
-            message: "Contribution Guidelines:",
-            name: "guidelines"
-        },
-        {
-            type: "input",
-            message: "Test Instructins:",
-            name: "test",
-            default: "npm test"
-        },
-        {
-            type: "input",
-            message: "GitHub Username:",
-            name: "username"
-        },
-        {
-            type: "input",
-            message: "Email Address:",
-            name: "email"
-        }
-    ])
+const userAnswers = [
+    {
+        type: "input",
+        message: "Title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation Instructions:",
+        name: "installation",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "Usage Information:",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "License:",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPLv3", "BSD 3--Clause", "none"]
+    },
+    {
+        type: "input",
+        message: "Contribution Guidelines:",
+        name: "guidelines"
+    },
+    {
+        type: "input",
+        message: "Test Instructins:",
+        name: "test",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        message: "GitHub Username:",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Email Address:",
+        name: "email"
+    }
+]
 
-        .then(function (response) {
+function userInput(getAnswers, badge) {
 
-            const fileName = response.title.toLowerCase().split(' ').join('') + "_README.md";
-            const badge = response.license.split(' ').join('%20')
-
-            const userInput =
-                `# ${response.title}
-        
-
+    return `# ${getAnswers.title}
+    
+    
 ![License](https://img.shields.io/badge/License-${badge}-blue.svg)
-                
+
 ## Description
 
-${response.description}
+${getAnswers.description}
 
 
 ## Table of Contents
@@ -89,25 +84,25 @@ ${response.description}
 
 To install necessary dependencies, run the following command:
 
-\`\`\` ${response.installation} \`\`\`
+\`\`\` ${getAnswers.installation} \`\`\`
 
 
 
 ## Usage
 
-${response.usage}
+${getAnswers.usage}
 
 
 
 ## License
 
-This project is licensed under the ${response.license} license.
+This project is licensed under the ${getAnswers.license} license.
 
 
 
 ## Contribution Guidelines
 
-${response.guidelines}
+${getAnswers.guidelines}
 
 
 
@@ -115,24 +110,32 @@ ${response.guidelines}
 
 To run tests, run the following command:
 
-\`\`\` ${response.test} \`\`\`
+\`\`\` ${getAnswers.test} \`\`\`
 
 
 
 ## Questions
 
-If you have any further questions, you can reach me directly here: ${response.email}
+If you have any further questions, you can reach me directly here: ${getAnswers.email}
 
-You can find more of my work at [https://github.com/${response.username}/](https://github.com/${response.username}/).
+You can find more of my work at [https://github.com/${getAnswers.username}/](https://github.com/${getAnswers.username}/).
 `
-
-            fs.writeFile(fileName, userInput, function (err) {
-                if (err) {
-                    console.log(err);
-                }
-                console.log("Success!")
-            })
-        })
-
 }
-createReadme();
+
+async function createReadme() {
+    try {
+        const getAnswers = await inquirer.prompt(userAnswers);
+        const badge = await getAnswers.license.split(' ').join('%20');
+        const fileName = await getAnswers.title.toLowerCase().split(' ').join('') + "_README.md";
+        const createFile = await userInput(getAnswers, badge);
+        fs.writeFile(fileName, createFile, function (err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Your README has been created!")
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+createReadme()
